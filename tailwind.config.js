2f16e18cf694902b6d555ba0d57227a18a72c25d
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-02-13 18:35:16 +0530

    completed login page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["SF Pro Display"],
+    },
+    colors: {
+      "primary-text": "#303030",
+      "secondary-text": "#222222",
+    },
+  },
+  plugins: [],
+};
