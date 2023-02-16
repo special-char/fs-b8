9133516450b8b5deed89330c2cd98851c077ce7b
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-02-16 18:26:37 +0530

    commit code

---
+++ str1.js
@@ -0,0 +1,53 @@
+const firstName = "Yagnesh";
+
+const lastName = "Modh";
+
+const fullName = firstName + " " + lastName;
+
+const designation = "Founder";
+
+console.log(fullName);
+
+// Yagnesh's Car
+
+const pos = firstName + "'s car";
+
+console.log(pos);
+
+// yagnesh modh
+// Founder
+
+const info = firstName + " " + lastName + "\t" + designation;
+
+console.log(info);
+
+const title = "Portfolio";
+
+const html =
+  "<!DOCTYPE html>" +
+  '\n<html lang="en">' +
+  "\n<head>" +
+  '\n\t<meta charset="UTF-8">' +
+  '\n\t<meta http-equiv="X-UA-Compatible" content="IE=edge">' +
+  '\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+  "\n\t<title>" +
+  title +
+  "</title>" +
+  "\n</head>" +
+  "\n<body>" +
+  "\n" +
+  "\n</body>" +
+  "\n</html>";
+
+console.log(html);
+
+// Yagnesh Modh
+
+// const
+
+// var a = 1;
+
+// console.log(a);
+
+// a = "jkahsdfkljhadsfm ,s lakshdflkasdjf lkajsdlkfjadslfkj"
+// console.log(a);
---
+++ str2.js
@@ -0,0 +1,40 @@
+const firstName = "Yagnesh";
+
+const lastName = "Modh";
+
+const fullName = `${firstName} ${lastName}`;
+
+const designation = "Founder";
+
+console.log(fullName);
+
+// Yagnesh's Car
+
+const pos = `${firstName}'s car`;
+
+console.log(pos);
+
+// yagnesh modh
+// Founder
+
+const info = `${firstName} ${lastName}
+${designation}`;
+
+console.log(info);
+
+const title = "Portfolio";
+
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+</head>
+<body>
+    
+</body>
+</html>`;
+
+console.log(html);
