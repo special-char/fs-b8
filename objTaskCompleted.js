a909ead26b182d34142612e8caf378921a899b45
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-02-22 18:28:32 +0530

    commit code

---
+++ objTaskCompleted.js
@@ -0,0 +1,50 @@
+// 1. create a object named user and add following properties
+
+// name, age: 33
+
+// 2. add gender with mutable and immutable approch
+
+// 3. change age from 33 to 30 using both approch
+
+// 4. create a variable name key with value gender
+
+// 5. read using key
+
+// 6. remove age using both approch
+
+const user = {
+  name: "yagnesh",
+  age: 33,
+};
+
+// user.gender =  "male";
+
+// console.log(user);
+
+// const newUser = {...user, gender: 'male'};
+
+// console.log(newUser);
+
+// user.age = 30;
+
+// console.log(user);
+
+// const newUser = {...user, age: 30};
+
+// console.log(newUser);
+
+// console.log(user);
+
+// const key = 'gender'
+
+// console.log(user[key]);
+
+// delete user.age
+
+// console.log(user);
+
+// const { age, ...rest} = user;
+
+// console.log(rest);
+
+// console.log(user);
