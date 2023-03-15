baf70414927bf98b9543d8a897676cec88c936af
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-03-15 18:29:10 +0530

    commit code

---
+++ async.js
@@ -0,0 +1,33 @@
+// callback
+
+// promise
+
+// generator
+
+// document.addEventListener("click", () => {
+
+// })
+
+// console.log("s1")
+
+// setTimeout(() => {
+//     console.log("a1")
+//  }, 800)
+
+//  console.log("s2")
+
+//  setTimeout(() => {
+//     console.log("a2")
+//  }, 500)
+
+//  console.log("s3")
+
+// 5 Minute
+
+console.log("s1");
+
+setTimeout(() => {
+  console.log("a1");
+}, 0);
+
+console.log("s2");
