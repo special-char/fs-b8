e42a5996d7f0f06bcedb38ffd31c76da4414902e
Dharmi patel <122797478+Dharmi-Patel@users.noreply.github.com>
2023-03-23 15:48:22 +0530

    html files

---
+++ portfolio/const.js
@@ -0,0 +1,30 @@
+const
+// const myObj = { a: 1, b: 2 };
+// if (myObj.hasOwnProperty('a')) {
+//     console.log('myObj has a property called "a"');
+// }
+// const newobj = {
+//     a: 1,
+//     b: 2
+// };
+// newobj.c = 3;
+// console.log(newobj);
+
+// const newObj = { a: 1, b: 2, c: 3 };
+//  delete myObj.c;
+//  console.log(myObj); 
+
+//  const obj1 = { a: 10, b: 40 };
+//  const obj2 = { c: 33, d: 46 };
+// const mergedObj = Object.assign({}, obj1, obj2);
+//  console.log(mergedObj);
+
+// const myObj = {
+//     a: 1,
+//     b: 5
+// };
+// const newobj = Object.assign({}, myObj);
+// newobj.b = 10;
+// console.log(myObj)
+// console.log(newobj)
+
