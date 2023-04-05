25402c0779b54e0ae3d934e2561233566ef1c0da
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-04-05 15:53:52 +0530

    commit code

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,27 +1,78 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CloseIcon from '../../../assets/icons/close.svg'
 
-function Input({ id, placeholder, error, ...props }) {
-  console.log(props)
-  return (
-    <div>
-      <label className="sr-only" htmlFor={id}>
-        {placeholder}
-      </label>
-      <div className="input-wrapper">
-        <CloseIcon height={24} width={24} fill="red" className="prefix-icon" />
-        <input
-          id={id}
-          placeholder={placeholder}
-          className="input"
-          type="text"
-          {...props}
-        />
-        <CloseIcon height={24} width={24} fill="red" className="sufix-icon" />
+export default class Input extends Component {
+  state = {
+    text: '',
+  }
+
+  render() {
+    const {
+      id,
+      placeholder,
+      error,
+      prefixIcon: PrefixIcon,
+      suffixIcon: SuffixIcon,
+      ...props
+    } = this.props
+    return (
+      <div>
+        <label className="sr-only" htmlFor={id}>
+          {placeholder}
+        </label>
+        <div className="input-wrapper">
+          {PrefixIcon && (
+            <PrefixIcon height={24} width={24} className="prefix-icon" />
+          )}
+          <input
+            id={id}
+            placeholder={placeholder}
+            className="input"
+            type="text"
+            value={this.state.text}
+            onChange={event => {
+              // this.setState({ text: event.target.value })
+              this.setState({ text: event.target.value })
+            }}
+            style={{
+              borderColor: this.state.text.length > 20 ? 'red' : 'gray',
+              outlineColor: this.state.text.length > 20 ? 'red' : 'gray',
+            }}
+            {...props}
+          />
+          {SuffixIcon && (
+            <SuffixIcon height={24} width={24} className="sufix-icon" />
+          )}
+        </div>
+        {error && <p className="error">{error}</p>}
       </div>
-      {error && <p className="error">{error}</p>}
-    </div>
-  )
+    )
+  }
 }
 
-export default Input
+// import React from 'react'
+// import CloseIcon from '../../../assets/icons/close.svg'
+
+// function Input({ id, placeholder, error, ...props }) {
+//   return (
+//     <div>
+//       <label className="sr-only" htmlFor={id}>
+//         {placeholder}
+//       </label>
+//       <div className="input-wrapper">
+//         <CloseIcon height={24} width={24} fill="red" className="prefix-icon" />
+//         <input
+//           id={id}
+//           placeholder={placeholder}
+//           className="input"
+//           type="text"
+//           {...props}
+//         />
+//         <CloseIcon height={24} width={24} fill="red" className="sufix-icon" />
+//       </div>
+//       {error && <p className="error">{error}</p>}
+//     </div>
+//   )
+// }
+
+// export default Input
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
 import { createRoot } from 'react-dom/client'
 import Button from './components/Button'
 import '../styles/global.css'
 import Xyz from './components/Input'
 import Input from './components/Input'
 import CloseIcon from '../assets/icons/close.svg'
+import LockIcon from '../assets/icons/lock.svg'
+import ShowIcon from '../assets/icons/show.svg'
 
 // first letter of element should be capital letter
 // return single element from component
@@ -20,10 +22,39 @@ import CloseIcon from '../assets/icons/close.svg'
 //   )
 // }
 
+// let number = 1
+
+// function Counter() {
+//   return (
+//     <div>
+//       <Button
+//         variant="outline"
+//         text="+"
+//         onClick={() => {
+//           number = number + 1
+//           console.log(number)
+//         }}
+//       />
+//       <span id="number">{number}</span>
+//       <Button variant="outline" text="-" />
+//     </div>
+//   )
+// }
+
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
   <div>
+    <div>
+      <p className="text">Lorem, ipsum dolor.</p>
+      <p className="text">Lorem, ipsum dolor.</p>
+      <p className="text">Lorem, ipsum dolor.</p>
+      <p className="text">Lorem, ipsum dolor.</p>
+      <p className="text">Lorem, ipsum dolor.</p>
+      <p className="text">Lorem, ipsum dolor.</p>
+      <p className="text">Lorem, ipsum dolor.</p>
+    </div>
+
     <Button text="Learn" disabled />
     <Button text="Learn" icon={CloseIcon} />
     <Button text="Learn" variant="outline" icon={CloseIcon} />
@@ -33,17 +64,8 @@ root.render(
       name="firstName"
       placeholder="First Name"
       autoComplete="given-name"
+      prefixIcon={LockIcon}
+      suffixIcon={ShowIcon}
     />
-
-    <Input
-      id="lastName"
-      name="lastName"
-      placeholder="Last Name"
-      autoComplete="family-name"
-      type="password"
-      error="Something went wrong..."
-    />
-
-    <Input name="isMarried" type="checkbox" />
   </div>,
 )
