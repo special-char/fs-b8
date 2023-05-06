f4faaf9509456fd6fca79892ec82821b8027d1b8
ahir yogesh <ahiryogesh721@gmail.com>
2023-05-06 16:05:31 +0530

    clearall

--- app.js
+++
@@ -1,8 +0,0 @@
-const a = 5;
-
-const b = 11;
-
-module.exports = {
-  a,
-  b,
-};
--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import { createRoot } from 'react-dom/client'
-import App1 from '././components/todo/App1'
-
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App1 />)
